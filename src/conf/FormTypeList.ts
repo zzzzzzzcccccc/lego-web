01d654b4699260c5a94c1e9a1af5493e547ff48a
zc <z304307393@163.com>
2020-06-04 14:16:56 +0800

    first commit

--- src/conf/FormTypeList.ts
+++ src/conf/FormTypeList.ts
@@ -1,33 +1,21 @@
 export interface IFormType {
   title?: string;
-  list: Array<{ title: string, icon?: string }>;
+  list: Array<{ title: string, icon?: string, comp?: string }>;
 }
 
 const FormTypeList:Array<IFormType> = [
   {
-    title: '表单容器',
-    list: [
-      {title: '单行容器'},
-      {title: '左右列容器'},
-      {title: '上下列容器'},
-      {title: '居中容器'},
-    ]
-  },
-  {
     title: '表单控件',
     list: [
-      {title: '输入框'},
-      {title: '多行输入框'},
-      {title: '数字输入框'},
-      {title: '单选框'},
-      {title: '多选框'},
-      {title: '下拉框'},
-      {title: '年月日选择器'},
-      {title: '年月日时分秒选择器'},
-      {title: '年月日时间范围选择器'},
-      {title: '年月日时分秒范围选择器'}
+      {title: '输入框', comp: 'Input'},
+      {title: '多行输入框', comp: 'TextArea'},
+      {title: '数字输入框', comp: 'InputNumber'},
+      {title: '单选框', comp: 'Radio'},
+      {title: '多选框', comp: 'CheckBox'},
+      {title: '下拉框', comp: 'Select'},
+      {title: '时间选择器', comp: 'DatePicker'},
     ]
-  }
+  },
 ];
 
 export default FormTypeList;
